35c13cf25ee1b55b0c9aa2c8a2f866b7023fb1c5
Kalutu Daniel <78742463+Kalutu@users.noreply.github.com>
2023-07-04 16:50:29 +0300

    modified:   logout.php

--- logout.php
+++ logout.php
@@ -1,7 +1,6 @@
 <?php
 	include('connection.php');
 	session_start();
-	session_unset($_SESSION['username']);
-	session_unset($_SESSION['member_id']);
+	session_unset();
 	header('location:index.php');
-	?>
+?>
