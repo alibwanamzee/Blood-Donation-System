0079c253d5cd1578c38cf5347e547ff23d2e701f
georgesroberto <georgesroberto21@gmail.com>
2023-07-10 15:41:37 +0300

    Header.php

--- admin/template/header.php
+++ admin/template/header.php
@@ -7,34 +7,37 @@
 ?>
 <!doctype html>
 <html lang="en">
-
 <head>
 	<title>Welcome To Dashboard</title>
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
+	
 	<!-- VENDOR CSS -->
 	<link rel="stylesheet" href="assets/vendor/bootstrap/css/bootstrap.min.css">
 	<link rel="stylesheet" href="assets/vendor/font-awesome/css/font-awesome.min.css">
 	<link rel="stylesheet" href="assets/vendor/linearicons/style.css">
 	<link rel="stylesheet" href="assets/vendor/chartist/css/chartist-custom.css">
+	
 	<!-- MAIN CSS -->
 	<link rel="stylesheet" href="assets/css/main.css">
+	
 	<!-- FOR DEMO PURPOSES ONLY. You should remove this in your project -->
 	<!-- <link rel="stylesheet" href="assets/css/demo.css"> -->
+	
 	<!-- GOOGLE FONTS -->
 	<link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700" rel="stylesheet">
+	
 	<!-- ICONS -->
 	<link rel="apple-touch-icon" sizes="76x76" href="assets/img/apple-icon.png">
 	<link rel="icon" type="image/png" sizes="96x96" href="assets/img/favicon.png">
+	
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 	<script type="text/javascript" src="//cdn.datatables.net/1.10.16/js/jquery.dataTables.min.js"></script>
 	<link rel="stylesheet" type="text/css" href="//cdn.datatables.net/1.10.16/css/jquery.dataTables.min.css">
 	
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script> 
-	
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script> 
 </head>
-
 <body>
 	<!-- WRAPPER -->
 	<div id="wrapper">
@@ -82,7 +85,7 @@
 						<li class="dropdown">
 							<a href="#" class="dropdown-toggle" data-toggle="dropdown"><img src="assets/img/user.png" class="img-circle" alt="Avatar"> <span><?php echo $_SESSION['name'];?></span> <i class="icon-submenu lnr lnr-chevron-down"></i></a>
 							<ul class="dropdown-menu">
-								<li><a href="" data-toggle="modal" data-target="#myProfile"><i class="lnr lnr-user"></i> <span>My Profile</span></a></li>
+								<li><a href="" data-toggle="modal" data-target="profile.php"><i class="lnr lnr-user"></i> <span>My Profile</span></a></li>
 								<li><a href="#"><i class="lnr lnr-envelope"></i> <span>Message</span></a></li>
 								<li><a href="#"><i class="lnr lnr-cog"></i> <span>Settings</span></a></li>
 								<li><a href="logout.php"><i class="lnr lnr-exit"></i> <span>Logout</span></a></li>
@@ -104,7 +107,8 @@
 						<li><a href="panels.html" class=""><i class="lnr lnr-cog"></i> <span>Panels</span></a></li>
 						<li><a href="notifications.html" class=""><i class="lnr lnr-alarm"></i> <span>Notifications</span></a></li>
 						<li>
-							<a href="#subPages" data-toggle="collapse" class="collapsed"><i class="lnr lnr-file-empty"></i> <span>Setting</span> <i class="icon-submenu lnr lnr-chevron-left"></i></a>
+							<a href="#subPages" data-toggle="collapse" class="collapsed"><i class="lnr lnr-file-empty"></i>Here's the continuation:
+							<span>Setting</span> <i class="icon-submenu lnr lnr-chevron-left"></i></a>
 							<div id="subPages" class="collapse ">
 								<ul class="nav">
 									<li><a href="state.php" class="">State</a></li>
@@ -113,8 +117,6 @@
 								</ul>
 							</div>
 						</li>
-
-
 						<li><a href="tables.html" class=""><i class="lnr lnr-dice"></i> <span>Tables</span></a></li>
 						<li><a href="typography.html" class=""><i class="lnr lnr-text-format"></i> <span>Typography</span></a></li>
 						<li><a href="icons.html" class=""><i class="lnr lnr-linearicons"></i> <span>Icons</span></a></li>
@@ -125,39 +127,38 @@
 
 		<!-- profile modal -->
 		<div class="modal fade" id="myProfile" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
-												<div class="modal-dialog">
-												<div class="modal-content">
-												<div class="modal-header">
-												<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-												<center><h2 class="modal-title" id="myModalLabel">Are You Sure?</h2></center>
-												</div>
-												<div class="modal-body">
-									<?php
-										$select = $connection->query("SELECT * FROM users WHERE member_id='".$_SESSION['member_id']."'");
-										$fetc = $select->fetch_array(); ?>
-
-									
-				
-												
-									<div class="container-fluid">
-									<form action="edit_profile.php?profile_id=<?php echo $fetc['member_id']?>" method="post">		 
-									<input class="form-control" name="name" id="name" type="text" value="<?php echo $fetc['name'];?>">
-									<br>
-									<input class="form-control" name="uname" id="uname" type="text" value="<?php echo $fetc['username'];?>">
-									<br>
-									<input class="form-control" name="password" id="password" type="text" value="<?php echo $fetc['password'];?>">
-									<br>
-									<input class="form-control" name="email" id="email" type="text" value="<?php echo $fetc['email'];?>">	
-									<input class="form-control" name="phone" id="phone" type="text" value="<?php echo $fetc['phone'];?>">
-									<?php	?>
-
-												</div> 
-												</div>
-												<div class="modal-footer">
-												<button type="button" class="btn btn-default" data-dismiss="modal"><span class="glyphicon glyphicon-remove"></span> Cancel</button>
-												<button type="submit" class="btn btn-warning"><span class="glyphicon glyphicon-edit"></span> Edit</button>
-												</div>
-												</form>	
-												</div>
-												</div>
-												</div>
+			<div class="modal-dialog">
+				<div class="modal-content">
+					<div class="modal-header">
+						<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+						<center><h2 class="modal-title" id="myModalLabel">Are You Sure?</h2></center>
+					</div>
+					<div class="modal-body">
+						<?php
+							$select = $connection->query("SELECT * FROM users WHERE member_id='".$_SESSION['member_id']."'");
+							$fetc = $select->fetch_array();
+						?>
+						<div class="container-fluid">
+							<form action="edit_profile.php?profile_id=<?php echo $fetc['member_id']?>" method="post">		 
+								<input class="form-control" name="name" id="name" type="text" value="<?php echo $fetc['name'];?>">
+								<br>
+								<input class="form-control" name="uname" id="uname" type="text" value="<?php echo $fetc['username'];?>">
+								<br>
+								<input class="form-control" name="password" id="password" type="text" value="<?php echo $fetc['password'];?>">
+								<br>
+								<input class="form-control" name="email" id="email" type="text" value="<?php echo $fetc['email'];?>">	
+								<input class="form-control" name="phone" id="phone" type="text" value="<?php echo $fetc['phone'];?>">
+								<?php	?>
+							</div> 
+						</div>
+						<div class="modal-footer">
+							<button type="button" class="btn btn-default" data-dismiss="modal"><span class="glyphicon glyphicon-remove"></span> Cancel</button>
+							<button type="submit" class="btn btn-warning"><span class="glyphicon glyphicon-edit"></span> Edit</button>
+						</div>
+					</form>	
+				</div>
+			</div>
+		</div>
+	</div>
+</body>
+</html>
