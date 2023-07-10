7e055604594f14c00e42fca55bcb2694744c14a6
georgesroberto <georgesroberto21@gmail.com>
2023-07-10 13:07:23 +0300

    Edited Donor Functionality

--- user_dashboard/add_donor.php
+++ user_dashboard/add_donor.php
@@ -3,51 +3,20 @@
 	session_start();
 
 	$name = $_POST['name'];
-	$fathername = $_POST['fathername'];
 	$gender = $_POST['gender'];
-	$datepicker = $_POST['datepicker'];
-	$weight = $_POST['weight'];
+	$phone = $_POST['phone'];
 	$email = $_POST['email'];
 	$state = $_POST['state'];
 	$city = $_POST['city'];
-	$pincode = $_POST['pincode'];
-	$phone = $_POST['phone'];
-	$address = $_POST['address'];
 
 
-	$fileInfo = PATHINFO($_FILES["photo"]["name"]);
+	$insert = $connection->query("INSERT INTO donor(name, gender, phone, email, state, city, username_fk, status) VALUES ('$name', '$gender', '$phone', '$email', '$state', '$city', '".$_SESSION['membername']."', '0')");
 	
-	if (empty($_FILES["photo"]["name"])){
-		$location="";
-	}
-	else{
-		if ($fileInfo['extension'] == "jpg" OR $fileInfo['extension'] == "png") {
-			$newFilename = $fileInfo['filename'] . "_" . time() . "." . $fileInfo['extension'];
-			move_uploaded_file($_FILES["photo"]["tmp_name"], "../upload/" . $newFilename);
-			$location = "upload/" . $newFilename;
-		}
-		else{
-			$location="";
-			?>
-				<script>
-					window.alert('Photo not added. Please upload JPG or PNG photo only!');
-				</script>
-			<?php
-		}
-	}
-
-// echo "<pre>";
-// 	print_r($_POST);
-// 	exit();
-
-	$insert = $connection->query("INSERT INTO donor(name, father_name, gender, dob, body_weight, email, state, city, address, pincode, phone, image, username_fk, status) VALUES ('$name', '$fathername', '$gender', '$datepicker', '$weight', '$email', '$state', '$city', '$pincode', '$phone', '$address', '$location', '".$_SESSION['membername']."', '0')");
-	// $r = "INSERT INTO donor(name, father_name, gender, dob, body_weight, email, state, city, address, pincode, phone, image, username_fk) VALUES ('$name', '$fathername', '$gender', '$datepicker', '$weight', '$email', '$state', '$city', '$pincode', '$phone', '$address', '$location',)";
-	// echo $r;
-	// exit();
-	
-	if($insert){
-		header('location:donor.php');
-	}else {
-		header('location:donor.php');
+	if ($insert) {
+		header('location:user_dashboard.php?status=success');
+		exit();
+	} else {
+		header('location:user_dashboard.php?status=error');
+		exit();
 	}
 ?>
