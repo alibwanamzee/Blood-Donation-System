22671e6bc6a9d7df87b8022717c8b935bb3d0657
Kalutu Daniel <78742463+Kalutu@users.noreply.github.com>
2023-07-11 12:29:37 +0300

    modified:   profile.php

--- profile.php
+++ profile.php
@@ -1,7 +1,7 @@
 <!doctype html>
 <html lang="en">
 <head>
-    <title>Profile | Klorofil - Free Bootstrap Dashboard Template</title>
+    <title>Profile | Klorofil</title>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
@@ -23,6 +23,7 @@
             background-color: #fff;
             padding: 20px;
             border-radius: 5px;
+            height: 70vh;
         }
     </style>
 </head>
@@ -31,52 +32,27 @@
         <div class="profile-header">
             <div class="profile-main">
                 <img src="assets/img/user-medium.png" class="img-circle" alt="Avatar">
-                <h3 class="name">Samuel Gold</h3>
+                <h3 class="name"></h3>
                 <span class="online-status status-available">Available</span>
             </div>
-            <div class="profile-stat">
-                <div class="row">
-                    <div class="col-md-4 stat-item">
-                        45 <span>Projects</span>
-                    </div>
-                    <div class="col-md-4 stat-item">
-                        15 <span>Awards</span>
-                    </div>
-                    <div class="col-md-4 stat-item">
-                        2174 <span>Points</span>
-                    </div>
-                </div>
-            </div>
+            
         </div>
         <div class="profile-detail">
             <div class="profile-info">
-                <h4 class="heading">Basic Info</h4>
+                <h3 class="heading">Basic Info</h3>
                 <ul class="list-unstyled list-justify">
                     <li>Birthdate <span>24 Aug, 2016</span></li>
                     <li>Mobile <span>(124) 823409234</span></li>
                     <li>Email <span>samuel@mydomain.com</span></li>
-                    <li>Website <span><a href="https://www.themeineed.com">www.themeineed.com</a></span></li>
+                    </span></li>
                 </ul>
-            </div>
-            <div class="profile-info">
-                <h4 class="heading">Social</h4>
-                <ul class="list-inline social-icons">
-                    <li><a href="#" class="facebook-bg"><i class="fa fa-facebook"></i></a></li>
-                    <li><a href="#" class="twitter-bg"><i class="fa fa-twitter"></i></a></li>
-                    <li><a href="#" class="google-plus-bg"><i class="fa fa-google-plus"></i></a></li>
-                    <li><a href="#" class="github-bg"><i class="fa fa-github"></i></a></li>
-                </ul>
-            </div>
-            <div class="profile-info">
-                <h4 class="heading">About</h4>
-                <p>Interactively fashion excellent information after distinctive outsourcing.</p>
             </div>
             <div class="text-center"><a href="#" class="btn btn-primary">Edit Profile</a></div>
         </div>
     </div>
     <footer>
         <div class="container-fluid">
-            <p class="text-center">&copy; 2017 <a href="https://www.themeineed.com" target="_blank">Theme I Need</a>. All Rights Reserved.</p>
+            <p class="text-center">&copy; 2023. All Rights Reserved.</p>
         </div>
     </footer>
     <script src="assets/vendor/jquery/jquery.min.js"></script>
