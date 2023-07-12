a5f42654eae9ea1394343da3715910c756d271f6
georgesroberto <georgesroberto21@gmail.com>
2023-07-12 10:56:50 +0300

    Edited Members

--- admin_dasboard/members.php
+++ admin_dasboard/members.php
@@ -14,7 +14,7 @@
 			<div class="main-content">
 				<div class="container-fluid">
 
-  <h2>Hello,  <span style="color: blue"> <?php echo $_SESSION['username']?></span> Manage Members Here. </h2> <br />
+  <h2>Hello,  <span style="color: blue"> <?php echo $_SESSION['username']?></span><br />Manage Donors</h2> <br />
   <p><button type="button" class="btn btn-primary" data-toggle="modal" data-target="#addmember">Add new</button></p> <br />           
   <table class="table table-bordered" id="members">
     <thead>
