13311fe0d8081df35522a35ec24bd033b3cb4a91
Kalutu Daniel <78742463+Kalutu@users.noreply.github.com>
2023-07-14 12:09:22 +0300

    Display edit

--- login.php
+++ login.php
@@ -8,24 +8,22 @@
 	$login = $connection->query("SELECT * FROM users WHERE username='$username' AND password='$password'");
 
 	$fetch = $login->fetch_array();
-	if($login->num_rows == 1){
-		if($fetch['usertype'] == 'admin'){
+	if ($login->num_rows == 1) {
+		if ($fetch['usertype'] == 'admin') {
 			$_SESSION['member_id'] = $fetch['member_id'];
 			$_SESSION['username'] = $fetch['username'];
 			header('location:admin_dasboard/admin_dashboard.php');
-
 		} elseif ($fetch['usertype'] == 'phlebotomists')  {
 			$_SESSION['userid'] = $fetch['user_id'];
 			$_SESSION['membername'] = $fetch['username'];
 			header('location:/Blood_donation_system/admin/home.php');
-
 		} elseif ($fetch['usertype'] == 'donor')  {
 			$_SESSION['userid'] = $fetch['user_id'];
 			$_SESSION['membername'] = $fetch['username'];
 			header('location:user_dashboard/user_dashboard.php');
 		} 
-	}else {
-		$_SESSION['error'] = '';
-		header('location:../index.php');	
+	} else {
+		$_SESSION['error'] = 'Invalid username or password. Please try again.';
+		echo '<script>alert("Invalid username or password. Please try again."); window.location.href = "index.php";</script>';
 	}
 ?>
