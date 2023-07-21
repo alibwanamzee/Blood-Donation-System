691e95fc56804dcd5c7172dab5434765e484b66d
georgesroberto <georgesroberto21@gmail.com>
2023-07-21 11:25:12 +0300

    Updated Donor Status

--- admin_dasboard/approve_donor.php
+++ admin_dasboard/approve_donor.php
@@ -4,6 +4,6 @@
 	$id = $_GET['status_id'];
 	$status = $_POST['status'];
 
-	$edit = $connection->query("UPDATE donor SET pends = 2, status='$status' WHERE donor_id='$id'");
-	header('location:approved_donors.php');
+	$edit = $connection->query("UPDATE donor SET pends=1, status=2 WHERE donor_id='$id'");
+	header('location:pending_donors.php');
 ?>
