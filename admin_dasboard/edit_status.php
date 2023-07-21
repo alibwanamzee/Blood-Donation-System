a834d96b3fb2a566f80ece6c3ad1d1728ae29498
Kalutu Daniel <78742463+Kalutu@users.noreply.github.com>
2023-07-21 16:49:01 +0300

    solved merge conflict

--- admin_dasboard/edit_status.php
+++ admin_dasboard/edit_status.php
@@ -5,6 +5,12 @@
 	$status = $_POST['status'];
 
 	$edit = $connection->query("UPDATE donor SET pends=1, status='$status' WHERE donor_id='$id'");
+	$decrementQuery = "UPDATE stats SET pending_req = pending_req - 1";
+    $connection->query($decrementQuery);
+	$incrementQuery = "UPDATE stats SET approved_req = approved_req + 1";
+    $connection->query($incrementQuery);
+
+
 	header('location:pending_donors.php');
 
 ?>
