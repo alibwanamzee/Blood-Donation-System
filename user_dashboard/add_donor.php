0607aa2b2e06b1240ba6afd7543b3c201020324f
georgesroberto <georgesroberto21@gmail.com>
2023-07-21 16:31:09 +0300

    Edited Stats Database

--- user_dashboard/add_donor.php
+++ user_dashboard/add_donor.php
@@ -11,9 +11,12 @@
 
 
 	$insert = $connection->query("INSERT INTO donor(name, gender, phone, email, state, city, username_fk, status) VALUES ('$name', '$gender', '$phone', '$email', '$state', '$city', '".$_SESSION['membername']."', '0')");
+
+	$insert2 = $connection->query("INSERT INTO (name, gender, phone, email, state, city, username_fk, status) VALUES ('$name', '$gender', '$phone', '$email', '$state', '$city', '".$_SESSION['membername']."', '0')");
 	
 	if ($insert) {
 		header('location:user_dashboard.php?status=success');
+		$pending_req = $connection->query("SELECT pending_req FROM stats" + 1); 
 		exit();
 	} else {
 		header('location:user_dashboard.php?status=error');
