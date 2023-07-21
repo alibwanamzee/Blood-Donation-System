e0fceb24540fa6d7eb6a3da1cc6a98fe260f1590
Kalutu Daniel <78742463+Kalutu@users.noreply.github.com>
2023-07-21 16:16:24 +0300

    Added the increment pneding_req functionality

--- user_dashboard/add_donor.php
+++ user_dashboard/add_donor.php
@@ -1,22 +1,25 @@
 <?php
-	include('../connection.php');
-	session_start();
+include('../connection.php');
+session_start();
 
-	$name = $_POST['name'];
-	$gender = $_POST['gender'];
-	$phone = $_POST['phone'];
-	$email = $_POST['email'];
-	$state = $_POST['state'];
-	$city = $_POST['city'];
+$name = $_POST['name'];
+$gender = $_POST['gender'];
+$phone = $_POST['phone'];
+$email = $_POST['email'];
+$state = $_POST['state'];
+$city = $_POST['city'];
 
+$insert = $connection->query("INSERT INTO donor(name, gender, phone, email, state, city, username_fk, status) VALUES ('$name', '$gender', '$phone', '$email', '$state', '$city', '".$_SESSION['membername']."', '0')");
 
-	$insert = $connection->query("INSERT INTO donor(name, gender, phone, email, state, city, username_fk, status) VALUES ('$name', '$gender', '$phone', '$email', '$state', '$city', '".$_SESSION['membername']."', '0')");
-	
-	if ($insert) {
-		header('location:user_dashboard.php?status=success');
-		exit();
-	} else {
-		header('location:user_dashboard.php?status=error');
-		exit();
-	}
+if ($insert) {
+    // Increment the pending_request value in the stats table
+    $incrementQuery = "UPDATE stats SET pending_req = pending_req + 1";
+    $connection->query($incrementQuery);
+
+    header('location:user_dashboard.php?status=success');
+    exit();
+} else {
+    header('location:user_dashboard.php?status=error');
+    exit();
+}
 ?>
