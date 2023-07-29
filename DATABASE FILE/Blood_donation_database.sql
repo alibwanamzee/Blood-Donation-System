869e628d4fa368b842c4f92f1ec85aaf141a18a6
Kalutu Daniel <78742463+Kalutu@users.noreply.github.com>
2023-07-29 15:55:10 +0300

    modified:   DATABASE FILE/Blood_donation_database.sql

--- DATABASE FILE/Blood_donation_database.sql
+++ DATABASE FILE/Blood_donation_database.sql
@@ -130,21 +130,24 @@ DROP TABLE IF EXISTS `donor`;
 
 CREATE TABLE `donor` (
   `donor_id` int(100) NOT NULL AUTO_INCREMENT,
-  `name` varchar(100) DEFAULT NULL,
   `gender` varchar(100) DEFAULT NULL,
   `datepicker` varchar(100) DEFAULT NULL,
   `body_weight` varchar(100) DEFAULT NULL,
-  `email` varchar(100) DEFAULT NULL,
   `blood_group` varchar(100) DEFAULT NULL,
   `state` varchar(100) DEFAULT NULL,
   `city` varchar(100) DEFAULT NULL,
   `phone` varchar(100) DEFAULT NULL,
-  `username_fk` varchar(100) NOT NULL,
+  `member_id` int(100) NOT NULL,
   `status` int(12) DEFAULT NULL,
   `pends` int(12) DEFAULT 0,
-  PRIMARY KEY (`donor_id`,`username_fk`),
-  KEY `users` (`username_fk`)
+  PRIMARY KEY (`donor_id`),
+  KEY `member_id` (`member_id`),
+  CONSTRAINT `fk_donor_member`
+    FOREIGN KEY (`member_id`)
+    REFERENCES `users` (`member_id`)
+    ON DELETE CASCADE
 ) ENGINE=InnoDB AUTO_INCREMENT=18 DEFAULT CHARSET=latin1;
+
 
 /*Table structure for table `email_subs` */
 
