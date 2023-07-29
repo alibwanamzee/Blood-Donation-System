80fe5e1fd450508d47f616978087d4f1292419c7
Kalutu Daniel <78742463+Kalutu@users.noreply.github.com>
2023-07-29 15:55:19 +0300

    modified:   user_dashboard/add_donor.php

--- user_dashboard/add_donor.php
+++ user_dashboard/add_donor.php
@@ -2,14 +2,12 @@
 include('../connection.php');
 session_start();
 
-$name = $_POST['name'];
 $gender = $_POST['gender'];
 $phone = $_POST['phone'];
-$email = $_POST['email'];
 $state = $_POST['state'];
 $city = $_POST['city'];
 
-$insert = $connection->query("INSERT INTO donor(name, gender, phone, email, state, city, username_fk, status) VALUES ('$name', '$gender', '$phone', '$email', '$state', '$city', '".$_SESSION['membername']."', '0')");
+$insert = $connection->query("INSERT INTO donor(gender, phone, state, city, member_id, status) VALUES ('$gender', '$phone', '$state', '$city', '".$_SESSION['id']."', '0')");
 
 if ($insert) {
     if (isset($_SESSION['id'])) {
