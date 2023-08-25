f538b0baf3cc1cbfa4e2692206ad107aca4acad1
unknown <georgesroberto21@gmail.com>
2023-08-25 15:43:04 +0300

    Updated UI

--- user_dashboard/user_header.php
+++ user_dashboard/user_header.php
@@ -11,7 +11,7 @@
 <html lang="en">
 
 <head>
-	<title>Welcome To Donor Dashboard</title>
+	<title>Donor Dashboard</title>
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
@@ -27,8 +27,8 @@
 	<!-- GOOGLE FONTS -->
 	<link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700" rel="stylesheet">
 	<!-- ICONS -->
-	<link rel="apple-touch-icon" sizes="76x76" href="assets/img/apple-icon.png">
-	<link rel="icon" type="image/png" sizes="96x96" href="assets/img/favicon.png">
+	<link rel="apple-touch-icon" sizes="76x76" href="assets/img/drop.png">
+	<link rel="icon" type="image/png" sizes="96x96" href="assets/img/drop.png">
 </head>
 
 <body>
